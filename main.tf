099e5a02f264705c772aebea0396c6c5812faf29
Dmytro <demon9709>
2025-10-21 18:28:02 +0300

    Add AzureRM backend and migrate state from local to remote

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
 terraform {
   required_version = ">= 1.8.4"
 
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = var.storage_account_name
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the existing Azure Storage Account for Terraform state"
 }
