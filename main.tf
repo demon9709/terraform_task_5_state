c3566d23729e47851c9d5e2f06f63dc0f801c222
Dmytro <demon9709>
2025-10-21 18:22:07 +0300

    Initial commit with local backend

--- main.tf
+++ main.tf
@@ -1,16 +1,11 @@
 terraform {
   required_version = ">= 1.8.4"
 
-  backend "azurerm" {
-    resource_group_name   = "tfstate"
-    storage_account_name  = "myteststoragefortr"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-    use_oidc              = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
 }
